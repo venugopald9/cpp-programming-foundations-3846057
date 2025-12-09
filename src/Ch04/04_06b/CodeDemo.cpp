ee1d3c961157f8bd981ebd1ee2e072b246030cba
venugopald9 <169176335+venugopald9@users.noreply.github.com>
2025-12-09 06:39:47 +0000

    Second day practice

--- src/Ch04/04_06b/CodeDemo.cpp
+++ src/Ch04/04_06b/CodeDemo.cpp
@@ -4,8 +4,18 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(){
+    std::vector<std::string> checkpoints = {"Start", "Forest", "Castle"};
+
+    checkpoints.push_back("Cave");
+    checkpoints.push_back("Finish");
+
+    std::cout << "The game has" << checkpoints.size() << "checkpoints" << std::endl;
+    std::cout << "The checkpoint at index 2 is " << checkpoints[2] << std::endl;
+    checkpoints[2] = "Dark Castle";
+    std::cout << "Checkpoint updated to: " << checkpoints[2] << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
