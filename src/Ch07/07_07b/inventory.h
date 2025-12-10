1048caed25d0cb88e4cb629d1a0d6482bb5e06fa
venugopald9 <169176335+venugopald9@users.noreply.github.com>
2025-12-10 09:32:42 +0000

    final commits

---
+++ src/Ch07/07_07b/inventory.h
@@ -0,0 +1,42 @@
+#pragma once //to implement this file only once
+
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class Inventory{
+public:
+    // Constructor
+    Inventory();
+
+    // Overloaded Constructor
+    Inventory(int capacity_i);
+
+    // Destructor
+    ~Inventory();
+
+    // Add item to inventory
+    void addItem(const std::string& item);
+
+    // Remove item from inventory
+    void removeItem(const std::string& item);
+
+    // Access item by index
+    std::string getItem(int index) const;
+
+    // Get number of items in the inventory
+    int getItemCount() const;
+
+    // Display inventory contents
+    void displayInventory() const;
+
+private:
+    std::vector<std::string> *items; // Pointer to a vector of items
+    int capacity; // Maximum number of items allowed
+};
+
+#endif // INVENTORY_H
